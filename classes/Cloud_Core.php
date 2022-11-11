d251b1a07e1a5665749b4acd35a25be8cd063420
Joachim Happel <joachim.happel@gmail.com>
2022-11-11 16:34:09 +0100

    compatible to lazy-blocks 3.0

--- classes/Cloud_Core.php
+++ classes/Cloud_Core.php
@@ -61,6 +61,7 @@ Class Cloud_Core{
 		//wp_enqueue_script('jquery-extra', self::$pluginurl.'js/jquery-3.6.0.min.js',null,'3.6.0',false);
 		wp_enqueue_style('fancytree_style', self::$pluginurl.'fancytree/skin-win8/ui.fancytree.min.css');
 		wp_enqueue_style('cloud_style', self::$pluginurl.'css/form.css');
+		wp_enqueue_style('admin_style', self::$pluginurl.'css/admin.css');
 		wp_enqueue_script('fancytree', self::$pluginurl.'fancytree/jquery.fancytree-all-deps.js',null,null,true);
 		wp_enqueue_script('cloud', self::$pluginurl.'js/cloud.js',null,false,true);
 		wp_enqueue_script('cloudframe', self::$pluginurl.'js/cloudframe.js',null,null,true);
--- rpicloud.php
+++ rpicloud.php
@@ -9,8 +9,16 @@ Author URI: https://github.com/johappel
 */
 
 require_once 'sabre/autoload.php';
-require_once 'classes/Cloud_Block.php';
 require_once 'classes/Cloud_Core.php';
+
+Cloud_Core::$pluginurl = plugin_dir_url (__FILE__);
+Cloud_Core::$plugindir = plugin_dir_path(__FILE__);
+Cloud_Core::$shorturl = home_url().'/cloud/';
+Cloud_Core::$frameurl = home_url().'/rpicloud/';
+Cloud_Core::$officeurl = home_url().'/cloudview/';
+Cloud_Core::$wpdir = ABSPATH;
+
+require_once 'classes/Cloud_Block.php';
 require_once 'classes/Cloud_Config.php';
 require_once 'classes/Cloud_Client.php';
 require_once 'classes/Cloud_Directory.php';
@@ -20,13 +28,6 @@ require_once 'classes/Cloud_Upload.php';
 require_once 'classes/Cloud_File.php';
 require_once 'classes/Cloud_Helper.php';
 require_once 'classes/Cloud_Template.php';
-
-Cloud_Core::$pluginurl = plugin_dir_url (__FILE__);
-Cloud_Core::$plugindir = plugin_dir_path(__FILE__);
-Cloud_Core::$shorturl = home_url().'/cloud/';
-Cloud_Core::$frameurl = home_url().'/rpicloud/';
-Cloud_Core::$officeurl = home_url().'/cloudview/';
-Cloud_Core::$wpdir = ABSPATH;
 
 define('RPICLOUD', 'rpi-virtuell Nextcloud Plugin');
 
